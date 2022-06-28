0e05733b882b69e59a8c809f1cc190202ccdcca3
Akash2377 <surveakash01@gmail.com>
2022-06-28 14:44:25 +0530

    home page completed

---
+++ index.js
@@ -0,0 +1,222 @@
+var restoData = [
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/a/j/p20996-145631488756cd9a0796608.jpg?tr=tr:n-large",
+    name: "Tamasha",
+    place: "Connaught Place, Central Delhi",
+    id: 1,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/b/t/p27452-15020105505986dcb6d147f.jpg?tr=tr:n-large",
+    name: "Local",
+    place: "Connaught Place, Central Delhi",
+    id: 2,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/u/y/p20941-15700828565d959028e9f28.jpg?tr=tr:n-large",
+    name: "Unplugged Courtyard",
+    place: "Connaught Place, Central Delhi",
+    id: 3,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/h/o/p420-15241335515ad86eaf373ce.jpg?tr=tr:n-large",
+    name: "WareHouse Cafe",
+    place: "Connaught Place, Central Delhi",
+    id: 4,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/e/p29906-16183962346076c44ac433d.jpg?tr=tr:n-large",
+    name: "QBA",
+    place: "Connaught Place, Central Delhi",
+    id: 5,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/m/u/p31393-15972091555f337a43bb961.jpg?tr=tr:n-large",
+    name: "The G.T. ROAD",
+    place: "Connaught Place, Central Delhi",
+    id: 6,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/6/s/m/p69146-163921872761b47e27e5f71.jpg?tr=tr:n-large",
+    name: "High 5 - Everyday",
+    place: "Connaught Place, Central Delhi",
+    id: 7,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/a/d/p32381-1495943585592a49a166fe7.jpg?tr=tr:n-large",
+    name: "Ministry Of Beer",
+    place: "Connaught Place, Central Delhi",
+    id: 8,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/j/y/p19748-16474299906231c96621c58.jpg?tr=tr:n-large",
+    name: "Open house Cafe",
+    place: "Connaught Place, Central Delhi",
+    id: 9,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/c/z/p77626-15833232035e5f98437554f.jpg?tr=tr:n-large",
+    name: "Connaught Clubhouse",
+    place: "Connaught Place, Central Delhi",
+    id: 10,
+  },
+];
+var restoData2 = [
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/u/o/p77794-162736902060ffae3c99c14.jpg?tr=tr:n-large",
+    name: "7 Old Town",
+    place: "Hauz khas, South Delhi",
+    id: 1,
+    discount: "Flat 20% Off the Total Bill",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/w/o/p72707-16093292005fec6a307afb2.jpg?tr=tr:n-large",
+    name: "Local heights",
+    place: "Punjab Bagh, West Delhi",
+    id: 2,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/u/y/p20941-15700828565d959028e9f28.jpg?tr=tr:n-large",
+    name: "Unplugged Courtyard",
+    place: "Connaught Place, Central Delhi",
+    id: 3,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/h/o/p420-15241335515ad86eaf373ce.jpg?tr=tr:n-large",
+    name: "WareHouse Cafe",
+    place: "Connaught Place, Central Delhi",
+    id: 4,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/e/p29906-16183962346076c44ac433d.jpg?tr=tr:n-large",
+    name: "QBA",
+    place: "Connaught Place, Central Delhi",
+    id: 5,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/m/u/p31393-15972091555f337a43bb961.jpg?tr=tr:n-large",
+    name: "The G.T. ROAD",
+    place: "Connaught Place, Central Delhi",
+    id: 6,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/6/s/m/p69146-163921872761b47e27e5f71.jpg?tr=tr:n-large",
+    name: "High 5 - Everyday",
+    place: "Connaught Place, Central Delhi",
+    id: 7,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/a/d/p32381-1495943585592a49a166fe7.jpg?tr=tr:n-large",
+    name: "Ministry Of Beer",
+    place: "Connaught Place, Central Delhi",
+    id: 8,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/j/y/p19748-16474299906231c96621c58.jpg?tr=tr:n-large",
+    name: "Open house Cafe",
+    place: "Connaught Place, Central Delhi",
+    id: 9,
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/c/z/p77626-15833232035e5f98437554f.jpg?tr=tr:n-large",
+    name: "Connaught Clubhouse",
+    place: "Connaught Place, Central Delhi",
+    id: 10,
+  },
+];
+var offerData = [
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/Nov/20/30offid.png?tr=tr:n-small",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/Aug/12/25offid.png?tr=tr:n-small",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/Aug/12/20offid.png?tr=tr:n-small",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/Nov/20/15offid.png?tr=tr:n-small",
+  },
+];
+displayData(restoData);
+function displayData(restoData) {
+  restoData.map(function (el) {
+    var input1 = `<div class="hotelD">
+              <img class="hotelI" src="${el.image_url}" alt=""/>
+            </div>
+            <div id="textdata">
+              <div>
+                <div>
+                  <h4 class="headH">${el.name}</h4>
+                  <p class="paraP">${el.place}</p>
+                </div>
+                <div class="rating">4.2</div>
+              </div>
+              <div class = "discFooter">
+                <img class="percLogo"src="percentae_logo.png"/>  
+                Flat 30% Off the Total Bill
+              </div>
+            </div>`;
+    var div = document.createElement("div");
+    div.innerHTML = input1;
+    div.setAttribute("class", "singledata");
+    document.getElementById("restroData").append(div);
+  });
+}
+displayData2(restoData2);
+function displayData2(restoData2) {
+  restoData2.map(function (el) {
+    var input1 = `<div class="hotelD">
+              <img class="hotelI" src="${el.image_url}" alt=""/>
+            </div>
+            <div id="textdata">
+              <div>
+                <div>
+                  <h4 class="headH">${el.name}</h4>
+                  <p class="paraP">${el.place}</p>
+                </div>
+                <div class="rating">4.2</div>
+              </div>
+              <div class = "discFooter">
+                
+              </div>
+            </div>`;
+    var div = document.createElement("div");
+    div.innerHTML = input1;
+    div.setAttribute("class", "singledata");
+    document.getElementById("restroData2").append(div);
+  });
+}
+
+displayData3(offerData);
+function displayData3(offerData) {
+  offerData.map(function (el) {
+    var input3 = `<a href="" class="aoffer"><img src="${el.image_url}"alt=""/></a>`;
+    var div = document.createElement("div");
+    div.innerHTML = input3;
+    document.getElementById("OfferData").append(div);
+  });
+}
