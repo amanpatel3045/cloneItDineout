580a41e28a33317b1900ed5a138fc904f013608a
Akash2377 <surveakash01@gmail.com>
2022-06-29 09:58:40 +0530

    home page

--- index.js
+++ index.js
@@ -4,70 +4,70 @@ var restoData = [
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/a/j/p20996-145631488756cd9a0796608.jpg?tr=tr:n-large",
     name: "Tamasha",
     place: "Connaught Place, Central Delhi",
-    id: 1,
+    rating: "4.2",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/b/t/p27452-15020105505986dcb6d147f.jpg?tr=tr:n-large",
     name: "Local",
     place: "Connaught Place, Central Delhi",
-    id: 2,
+    rating: "4.0",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/u/y/p20941-15700828565d959028e9f28.jpg?tr=tr:n-large",
     name: "Unplugged Courtyard",
     place: "Connaught Place, Central Delhi",
-    id: 3,
+    rating: "4.1",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/h/o/p420-15241335515ad86eaf373ce.jpg?tr=tr:n-large",
     name: "WareHouse Cafe",
     place: "Connaught Place, Central Delhi",
-    id: 4,
+    rating: "4.0",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/e/p29906-16183962346076c44ac433d.jpg?tr=tr:n-large",
     name: "QBA",
     place: "Connaught Place, Central Delhi",
-    id: 5,
+    rating: "4.3",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/m/u/p31393-15972091555f337a43bb961.jpg?tr=tr:n-large",
     name: "The G.T. ROAD",
     place: "Connaught Place, Central Delhi",
-    id: 6,
+    rating: "4.3",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/6/s/m/p69146-163921872761b47e27e5f71.jpg?tr=tr:n-large",
     name: "High 5 - Everyday",
     place: "Connaught Place, Central Delhi",
-    id: 7,
+    rating: "3.9",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/a/d/p32381-1495943585592a49a166fe7.jpg?tr=tr:n-large",
     name: "Ministry Of Beer",
     place: "Connaught Place, Central Delhi",
-    id: 8,
+    rating: "4.0",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/j/y/p19748-16474299906231c96621c58.jpg?tr=tr:n-large",
     name: "Open house Cafe",
     place: "Connaught Place, Central Delhi",
-    id: 9,
+    rating: "4.1",
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/c/z/p77626-15833232035e5f98437554f.jpg?tr=tr:n-large",
     name: "Connaught Clubhouse",
     place: "Connaught Place, Central Delhi",
-    id: 10,
+    rating: "4.2",
   },
 ];
 var restoData2 = [
@@ -76,71 +76,70 @@ var restoData2 = [
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/u/o/p77794-162736902060ffae3c99c14.jpg?tr=tr:n-large",
     name: "7 Old Town",
     place: "Hauz khas, South Delhi",
-    id: 1,
-    discount: "Flat 20% Off the Total Bill",
+    rating: 3.9,
   },
   {
     image_url:
       "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/w/o/p72707-16093292005fec6a307afb2.jpg?tr=tr:n-large",
     name: "Local heights",
     place: "Punjab Bagh, West Delhi",
-    id: 2,
+    rating: 3.9,
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/u/y/p20941-15700828565d959028e9f28.jpg?tr=tr:n-large",
-    name: "Unplugged Courtyard",
-    place: "Connaught Place, Central Delhi",
-    id: 3,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/u/o/p77794-162736902060ffae3c99c14.jpg?tr=tr:n-large",
+    name: "7 Old Town",
+    place: "Hauz khas, South Delhi",
+    rating: 4.1,
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/h/o/p420-15241335515ad86eaf373ce.jpg?tr=tr:n-large",
-    name: "WareHouse Cafe",
-    place: "Connaught Place, Central Delhi",
-    id: 4,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/b/l/p106533-165518628462a8236cdb393.jpg?tr=tr:n-large",
+    name: "Deja Brew",
+    place: "Greater Kailash-2, Central Delhi",
+    rating: "5.0",
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/e/p29906-16183962346076c44ac433d.jpg?tr=tr:n-large",
-    name: "QBA",
-    place: "Connaught Place, Central Delhi",
-    id: 5,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/t/v/p36550-16541768756298bc6b9ba85.jpg?tr=tr:n-large",
+    name: "Code Bar and Kitchen",
+    place: "South Extension-1, South Delhi",
+    rating: 4.5,
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/m/u/p31393-15972091555f337a43bb961.jpg?tr=tr:n-large",
-    name: "The G.T. ROAD",
-    place: "Connaught Place, Central Delhi",
-    id: 6,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/e/i/p103367-165311182462887c100f3e7.jpg?tr=tr:n-large",
+    name: "Oh So Pro",
+    place: "Sector 46, Noida",
+    rating: 4.4,
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/6/s/m/p69146-163921872761b47e27e5f71.jpg?tr=tr:n-large",
-    name: "High 5 - Everyday",
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/y/w/p19259-15109933985a0fedf62bc72.jpg?tr=tr:n-large",
+    name: "Desi Vellagio - Village",
     place: "Connaught Place, Central Delhi",
-    id: 7,
+    rating: 4.1,
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/3/a/d/p32381-1495943585592a49a166fe7.jpg?tr=tr:n-large",
-    name: "Ministry Of Beer",
-    place: "Connaught Place, Central Delhi",
-    id: 8,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/h/k/p100874-16363602766188e0544df7d.jpg?tr=tr:n-large",
+    name: "Mugz Cafe",
+    place: "Gwal Pahari, Gurgaon",
+    rating: "4.0",
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/j/y/p19748-16474299906231c96621c58.jpg?tr=tr:n-large",
-    name: "Open house Cafe",
-    place: "Connaught Place, Central Delhi",
-    id: 9,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/t/t/p40798-164568864662173746cb471.jpg?tr=tr:n-large",
+    name: "OLLY-Olive's All Day Cafe",
+    place: "DLF Cyber City, Gurgaon",
+    rating: 4.3,
   },
   {
     image_url:
-      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/c/z/p77626-15833232035e5f98437554f.jpg?tr=tr:n-large",
-    name: "Connaught Clubhouse",
-    place: "Connaught Place, Central Delhi",
-    id: 10,
+      "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/b/x/p75132-15774433505e05e016b47cf.jpg?tr=tr:n-large",
+    name: "Breath - Fine Lounge",
+    place: "Punjabi Bagh, North Delhi",
+    rating: "4.0",
   },
 ];
 var offerData = [
@@ -161,6 +160,48 @@ var offerData = [
       "https://im1.dineout.co.in/images/uploads/misc/2020/Nov/20/15offid.png?tr=tr:n-small",
   },
 ];
+var buttonData = [
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/bffet.png",
+    name: "Buffet",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/pocket_friendly.png",
+    name: "Pocket Friendly",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/pet_friendly.png",
+    name: "Pet Friendly",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/must_visit.png",
+    name: "Must Visit",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/family_time.png",
+    name: "Family Time",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/pure_veg.png",
+    name: "Pure Veg",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/new_on_dineout.png",
+    name: "New On Dineout",
+  },
+  {
+    image_url:
+      "https://im1.dineout.co.in/images/uploads/misc/2020/May/8/happiest_hour.png",
+    name: "Happiest Hours",
+  },
+];
 displayData(restoData);
 function displayData(restoData) {
   restoData.map(function (el) {
@@ -173,11 +214,11 @@ function displayData(restoData) {
                   <h4 class="headH">${el.name}</h4>
                   <p class="paraP">${el.place}</p>
                 </div>
-                <div class="rating">4.2</div>
+                <div class="rating">${el.rating}</div>
               </div>
               <div class = "discFooter">
                 <img class="percLogo"src="percentae_logo.png"/>  
-                Flat 30% Off the Total Bill
+                Flat 10% Off the Total Bill
               </div>
             </div>`;
     var div = document.createElement("div");
@@ -198,11 +239,12 @@ function displayData2(restoData2) {
                   <h4 class="headH">${el.name}</h4>
                   <p class="paraP">${el.place}</p>
                 </div>
-                <div class="rating">4.2</div>
+                <div class="rating">${el.rating}</div>
               </div>
               <div class = "discFooter">
-                
-              </div>
+                <img class="percLogo"src="percentae_logo.png"/>  
+                Flat 20% Off the Total Bill
+              </div>          
             </div>`;
     var div = document.createElement("div");
     div.innerHTML = input1;
@@ -219,4 +261,45 @@ function displayData3(offerData) {
     div.innerHTML = input3;
     document.getElementById("OfferData").append(div);
   });
+}
+displayData4(buttonData);
+function displayData4(buttonData) {
+  buttonData.map(function (el) {
+    var input3 = ` <a class="buttonS" href=""> <div><img src="${el.image_url}" alt=""></div><div> <p>${el.name}</p></div></a>`;
+    var div = document.createElement("div");
+    div.innerHTML = input3;
+    document.getElementById("buttongrid").append(div);
+  });
+}
+function slide(direction) {
+  var container = document.getElementById("container");
+
+  scrollCompleted = 0;
+  var slideVar = setInterval(function () {
+    if (direction == "left") {
+      container.scrollLeft -= 28;
+    } else {
+      container.scrollLeft += 28;
+    }
+    scrollCompleted += 10;
+    if (scrollCompleted >= 100) {
+      window.clearInterval(slideVar);
+    }
+  }, 50);
+}
+function slide2(direction) {
+  var container = document.getElementById("container2");
+
+  scrollCompleted = 0;
+  var slideVar = setInterval(function () {
+    if (direction == "left") {
+      container.scrollLeft -= 28;
+    } else {
+      container.scrollLeft += 28;
+    }
+    scrollCompleted += 10;
+    if (scrollCompleted >= 100) {
+      window.clearInterval(slideVar);
+    }
+  }, 50);
 }
